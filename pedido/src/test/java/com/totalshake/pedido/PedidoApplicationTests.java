8e7167c5190fb75efba3f60a8590aac63af3dcfd
sarahp_cit <sarahp@ciandt.com>
2022-08-08 11:18:12 -0300

    criação do projeto

---
+++ pedido/src/test/java/com/totalshake/pedido/PedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalshake.pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
